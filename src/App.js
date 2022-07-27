e2f7c3385d8debf8e6a3fc3338153b7715b68f71
camicamposc <clearca57@gmail.com>
2022-07-27 10:29:42 -0400

    inputs de logueo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import LogIn from "./component/LogIn";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LogIn />} />
         <Route path="about" element={<About />} />
       </Routes>
     </div>
   );
 }
 
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
+// function Home() {
+//   return (
+//     <>
+//       <main>
+//         <h2>Welcome to the homepage!</h2>
+//         <p>You can do this, I believe in you.</p>
+//       </main>
+//       <nav>
+//         <Link to="/about">About</Link>
+//       </nav>
+//     </>
+//   );
+// }
 
 function About() {
   return (
